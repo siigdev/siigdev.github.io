c965e939301c135e675b1ee11e73ee9fa650cc9c
siigdev <sebbahlol@gmail.com>
2019-01-22 18:47:50 +0100

    Portfolio prototyping

---
+++ scripts/portfolio.js
@@ -0,0 +1,13 @@
+$(function () {
+    var selectedClass = "";
+    $(".btn").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio div").not("." + selectedClass).fadeOut();
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('scale-anm');
+            $("#portfolio").fadeTo(300, 1);
+        }, 300);
+
+    });
+});
