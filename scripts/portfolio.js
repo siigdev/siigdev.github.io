3a1db122800d52f202af145e597201a13192d07d
siigdev <sebbahlol@gmail.com>
2019-01-22 20:28:03 +0100

    Portfolio update page

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -2,7 +2,7 @@ $(function () {
     var selectedClass = "";
     $(".btn").click(function () {
         selectedClass = $(this).attr("data-rel");
-        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio").fadeTo(100, 0);
         $("#portfolio div").not("." + selectedClass).fadeOut();
         setTimeout(function () {
             $("." + selectedClass).fadeIn().addClass('scale-anm');
