5766438f29401fc2a66ab5bc2aaf893adf59ae54
siigdev <pwnage@live.dk>
2019-01-31 21:26:28 +0100

    Updated animation

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -4,7 +4,7 @@ $(function () {
         $("#typeselect>button.active").removeClass("active");
         $(this).addClass("active");
         selectedClass = $(this).attr("data-rel");
-        $("#portfolio").fadeTo(100, 0);
+        $("#portfolio").fadeTo(100, 1);
         $("#portfolio div").not("." + selectedClass).children().fadeOut();
         setTimeout(function () {
             $("." + selectedClass).find('*').fadeIn();
