564b38c3231c9e60b28c36f14168a056540c8fcb
siigdev <sebbahlol@gmail.com>
2019-02-04 21:20:21 +0100

    Adding markdown converter

--- scripts/writings.js
+++ scripts/writings.js
@@ -3,7 +3,12 @@ $(this.setContent = function () {
     xmlhttp.responseType = 'json';
     xmlhttp.onreadystatechange = function () {
         if (this.response != null) {
-            console.log(this.response.posts);
+            for(var i in this.response.posts){
+                var html = '<div class="post">' + this.response.posts[i].title + '</div>'
+                $(html).appendTo("#posts");
+                console.log(marked('# Marked in the browser\n\nRendered by **marked**.'));
+        }
+            
         }
     };
     xmlhttp.open("GET", "data/posts/config.json", true);
