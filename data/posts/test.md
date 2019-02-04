84b8494d5d2432aa17d35df7b9477d9835dc5f7b
siigdev <sebbahlol@gmail.com>
2019-02-04 21:54:18 +0100

    Functioning blogs

--- data/posts/test.md
+++ data/posts/test.md
@@ -1,5 +1,2 @@
 # Test
 test2
-
-<h1>Test</h1>
-<b>Test2
--- scripts/writings.js
+++ scripts/writings.js
@@ -1,16 +1,32 @@
-$(this.setContent = function () {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.responseType = 'json';
-    xmlhttp.onreadystatechange = function () {
-        if (this.response != null) {
-            for(var i in this.response.posts){
-                var html = '<div class="post">' + this.response.posts[i].title + '</div>'
-                $(html).appendTo("#posts");
-                console.log(marked('# Marked in the browser\n\nRendered by **marked**.'));
-        }
-            
-        }
+function App() {
+    this.setContent = function () {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.responseType = 'json';
+        xmlhttp.onreadystatechange = function () {
+            if (this.response != null) {
+                for (var i in this.response.posts) {
+                    let response = this.response.posts[i]
+                    var html = '<div class="post">' + response.title + '</div>'
+                    $(html).appendTo("#posts").one("click", function () {
+                        getContent(response.src);
+                    });
+                }
+            }
+        };
+        xmlhttp.open("GET", "data/posts/config.json", true);
+        xmlhttp.send();
     };
-    xmlhttp.open("GET", "data/posts/config.json", true);
-    xmlhttp.send();
-});
+    this.getContent = function (src) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.response != null) {
+                $(".content").html(marked(this.response))
+            }
+        }
+        xmlhttp.open("GET", "data/posts/"+ src, true);
+        xmlhttp.send();
+    };
+    this.setContent();
+
+}
+App();
