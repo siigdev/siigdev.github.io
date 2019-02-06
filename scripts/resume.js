140e0bca07c9330ea8077848fc276cd701181c33
siigdev <sebbahlol@gmail.com>
2019-02-06 21:29:20 +0100

    Fixed margin error in responsive menu

--- scripts/resume.js
+++ scripts/resume.js
@@ -2,7 +2,7 @@ $(function addResume(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.response != null) {
-            $(".content").html(marked(xmlhttp.response))
+            $(".resume").html(marked(xmlhttp.response))
         }
     };
     xmlhttp.open("GET", "data/posts/resume.md", true);
