f2ea53cf4cc27280fd0eb9ce16d428edd506cb9b
siigdev <sebbahlol@gmail.com>
2019-02-07 18:44:04 +0100

    Resume bugfix

--- scripts/resume.js
+++ scripts/resume.js
@@ -2,7 +2,7 @@ $(function addResume(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.response != null) {
-            $(".resume").html(marked(xmlhttp.response))
+            $(".content").html(marked(xmlhttp.response))
         }
     };
     xmlhttp.open("GET", "data/posts/resume.md", true);
