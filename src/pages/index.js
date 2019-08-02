e808526501732d268cf9e1e16a6592067a0d4f28
siigdev <sebbahlol@gmail.com>
2019-08-02 12:15:07 +0200

    Spellchecker

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ const IndexPage = () => (
     <SEO title="Home" />
     Hello! I'm Sebastian Nørgaard.
     <p>A Master student in Software Engineering at the University of Southern Denmark, Odense. 
-      Will be graduating in June, 2020. Generally enthusiastic about anything software-related, 
+      Will be graduating in June 2020. Generally enthusiastic about anything software-related, 
       and very dedicated to keep improving and learning new and exciting technologies.</p>
     <p>Currently looking out for a student job while I write my master thesis. Feel free to reach out to me if you have anything interesting.</p>
-    <p>You can find more information on <a href="https://github.com/siigdev">Github</a> or <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a></p>
-    <p>Or contact me through <a href="mailto:pwnage@live.dk">pwnage@live.dk</a></p>
+    <p>You can find more information on <a href="https://github.com/siigdev">GitHub</a> or <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a></p>
+    <p>Or contact me at <a href="mailto:pwnage@live.dk">pwnage@live.dk</a></p>
     <p></p>
   </Layout>
 )
