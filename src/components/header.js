4b1fbf61632bf5c1c8be0cd5eafa3188b6f8fac6
siigdev <sebbahlol@gmail.com>
2019-08-06 20:38:36 +0200

    Fixing the active links for subpages

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,19 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const isPartiallyActive = ({
+  isPartiallyCurrent
+}) => {
+  return isPartiallyCurrent
+    ? { className: "has-child active top" }
+    : null
+}
+
 const Header = ({ siteTitle }) => (
   <header>
     <div>
       <Link to="/" activeClassName="active">about</Link>
-      <Link to="/blog" activeClassName="active">blog</Link>
+      <Link to="/blog" getProps={isPartiallyActive} activeClassName="active">blog</Link>
     </div>
   </header>
 )
