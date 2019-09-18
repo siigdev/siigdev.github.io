c8f4e637fd68f1f7bc945fbf72bafa63b0920a60
siigdev <sebbahlol@gmail.com>
2019-09-18 18:38:08 +0200

    Important punctuation :)

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ const IndexPage = () => (
       Will be graduating in June 2020. Generally enthusiastic about anything software-related, 
       and very determined to continously improve and learn new and exciting technologies.</p>
     <p>Currently, I'm looking out for a student job while I write my master's thesis. Feel free to reach out to me if you have anything interesting. </p>
-    <p>You can find more information on <a href="https://github.com/siigdev">GitHub</a> or <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a></p>
-    <p>Or contact me at <a href="mailto:pwnage@live.dk">pwnage@live.dk</a></p>
+    <p>You can find more information on <a href="https://github.com/siigdev">GitHub</a> or <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a>.</p>
+    <p>Or contact me at <a href="mailto:pwnage@live.dk">pwnage@live.dk</a>.</p>
     <p></p>
   </Layout>
 )
