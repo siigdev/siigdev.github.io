ccf697e3cb7fd8419dfbaeda24a1cfce44c9ca72
Sebastian <45300947+siigdev@users.noreply.github.com>
2021-06-22 20:53:33 +0200

    Update index.js
    Update description

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <p>Hello! I'm Sebastian Nørgaard.</p>
-    <p>A Master student in Software Engineering at the University of Southern Denmark, Odense. 
-      Will be graduating in June 2020. Generally enthusiastic about anything software-related, 
-      and very determined to continously improve and learn new and exciting technologies.</p>
-    <p>I am currently working as an analyst for Netcompany while I write my master's thesis. Additionally, I'm also a part of the SDU Talent programme and I volunteer at Coding Pirates, Odense.</p>
+    <p>A Software Engineer currently located in Odense, Denmark. 
+      Generally enthusiastic about anything software-related, and very determined to continously improve and learn new and exciting technologies.</p>
+    <p>I am currently full-time employed as a consultant for Netcompany at the Ministry of Higher Education and Science. Additionally, I dedicate my time to different entrepreneurial projects.</p>
     <p>You can find more information on <a href="https://github.com/siigdev">GitHub</a> or <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a>.</p>
     <p>Or contact me at <a href="mailto:pwnage@live.dk">pwnage@live.dk</a>.</p>
     <p></p>
