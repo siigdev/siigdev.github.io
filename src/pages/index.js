781861752183ab9d74be78eeac6174ad603987c6
Sebastian Siig Nørgaard <sesn@netcompany.com>
2022-01-04 16:08:26 +0100

    Typo fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <p>Hello! I'm Sebastian Nørgaard.</p>
     <p>A Software Engineer located in Odense, Denmark. 
       Generally enthusiastic about anything software-related, and very determined to continously improve and learn new and exciting technologies.</p>
-    <p>I am currently full-time employed as a consultant for Netcompany at the Ministry of Higher Education and Science. Additionally, I dedicate my time to different entrepreneurial projects, and I am a CO-Founder of the disruptive multidestination travel engine, <a href="https://tryp.com">Tryp.com</a></p>
+    <p>I am currently full-time employed as a consultant for Netcompany at the Ministry of Higher Education and Science. Additionally, I dedicate my time to different entrepreneurial projects, and I am a co-founder of the disruptive multidestination travel engine, <a href="https://tryp.com">Tryp.com</a></p>
     <p>You can find more information on <a href="https://github.com/siigdev">GitHub</a> or <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a>.</p>
     <p>Or contact me at <a href="mailto:pwnage@live.dk">pwnage@live.dk</a>.</p>
     <p></p>
