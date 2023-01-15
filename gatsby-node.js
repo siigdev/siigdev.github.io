4ebfd595990836c9c6fe684ab05eea5390e03942
siigdev <sebbahlol@gmail.com>
2023-01-15 12:00:24 +0100

    Dependency updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,20 +16,17 @@ exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
   const blogPostTemplate = path.resolve(`src/templates/post.js`)
   return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
+  {
+    allMarkdownRemark(sort: {frontmatter: {date: DESC}}, limit: 1000) {
+      edges {
+        node {
+          fields {
+            slug
           }
         }
       }
     }
+  }
   `).then(result => {
     if (result.errors) {
       return Promise.reject(result.errors)
