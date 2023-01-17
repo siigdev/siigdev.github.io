cd3cc473e6713fb5c83e674d4b5e41a92893c96f
siigdev <sebbahlol@gmail.com>
2023-01-17 21:30:26 +0100

    Update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,12 @@ const IndexPage = () => (
     <SEO title="Home" />
     <p>Hello! I'm Sebastian Nørgaard.</p>
     <p>
-      A Software Engineer located in Odense, Denmark. Generally enthusiastic
-      about anything software-related, and very determined to continously
+      A Software Engineer located in Odense, Denmark. Generally passionate
+      about anything software-related, and highly dedicated to continously
       improve and learn new and exciting technologies.
     </p>
     <p>
-      I am currently full-time employed as a consultant for Netcompany at the
-      Ministry of Higher Education and Science. Additionally, I dedicate my time
-      to different entrepreneurial projects, and I am a co-founder of the
-      disruptive multidestination travel engine,{" "}
-      <a href="https://tryp.com">Tryp.com</a>
+      I am a founder of the disruptive multidestination online travel engine,{" "} <a href="https://tryp.com">Tryp.com</a>, where I dedicate my time managing the frontend development. I have prior professional experience in the full tech-stack working with large-scale, critical software systems for external clients. 
     </p>
     <p>
       You can find more information on{" "}
@@ -25,7 +21,7 @@ const IndexPage = () => (
       <a href="https://linkedin.com/in/sebastiannoergaard">LinkedIn</a>.
     </p>
     <p>
-      Or contact me at <a href="mailto:pwnage@live.dk">pwnage@live.dk</a>.
+      Or contact me at <a href="mailto:pwnage@live.dk">sebastian@tryp.dk</a>.
     </p>
     <p></p>
   </Layout>
